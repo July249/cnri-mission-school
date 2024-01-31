53ce436fc56fe3af90f5973242d5e50c3eca4237
jihyung.lee <jhlee7125@gmail.com>
2024-01-31 20:16:22 +0900

    add simple cli

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,21 @@
 #!/usr/bin/env node
 
-console.log('HELLO');
-console.log(process.argv);
+import { Command } from 'commander';
+
+const program = new Command();
+
+program.name('My CLI').description('My CLI description');
+
+program.command('todo')
+    .description('Add a new todo to Things app')
+    .argument('<todo>', 'todo text to add')
+    .option('-t, --today', 'is this for today?')
+    .action((todo, options) => {
+        console.log('todo!', { todo, options});
+    });
+
+program.parse();
+
 // when you type this in terminal: yarn run hello world
 // process.argv[0] => node interpreter (ex. /usr/local/bin/node)
 // process.argv[1] => path to this file (ex. /Users/username/Projects/CLI/src/cli.js)
