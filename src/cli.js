18a63e0a838e76dcb173d9db3486f7c0227a6974
jihyung.lee <jhlee7125@gmail.com>
2024-01-31 17:12:10 +0900

    process.argv description

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,9 @@
 #!/usr/bin/env node
 
 console.log('HELLO');
-
+console.log(process.argv);
+// when you type this in terminal: yarn run hello world
+// process.argv[0] => node interpreter (ex. /usr/local/bin/node)
+// process.argv[1] => path to this file (ex. /Users/username/Projects/CLI/src/cli.js)
+// process.argv[2] => first argument, command (ex. hello)
+// process.argv[3] => second argument, parameter (ex. world)
